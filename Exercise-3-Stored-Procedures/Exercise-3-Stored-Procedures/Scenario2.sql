2b8dd296fda8a15289f1f8fc25df09c6fea3b79b
PRADAKSHINA B <pradakshinab.23cse@kongu.edu>
2026-06-29 20:43:48 +0530

    PL_SQL 2

---
+++ Exercise-3-Stored-Procedures/Exercise-3-Stored-Procedures/Scenario2.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE PROCEDURE UpdateEmployeeBonus(
+    p_DepartmentID IN NUMBER,
+    p_BonusPercent IN NUMBER)
+AS
+BEGIN
+    UPDATE Employees
+    SET Salary = Salary + (Salary * p_BonusPercent/100)
+    WHERE DepartmentID = p_DepartmentID;
+    COMMIT;
+END;
+/
